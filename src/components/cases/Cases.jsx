78ff704a43f232a37aa517f61ba53d74d4585d85
peerawit <peerawit503@hmail.com>
2020-03-18 13:16:45 +0700

    cartrust

--- src/components/cases/Cases.jsx
+++ src/components/cases/Cases.jsx
@@ -408,7 +408,7 @@ const Cases = (props) => {
       }else if(singleCase.status === 'submit_book_transfer'){// if ส่งงานโอนทะเบียน sent car checkup
       var data = JSON.stringify({ tracking: 'car_check_up', user_id: props.user.id ,date:date, yes_no:checkCar.d1?"yes":"no"});
       console.log("incase 1");
-      }else if((singleCase.status === "transfer_doc_received" || singleCase.status === "deposit_doc_to_new_bank" || singleCase.status === "contact_customer")){
+      }else if((singleCase.status === "transfer_doc_received" || singleCase.status === "deposit_doc_to_new_bank")){
         console.log("incase 2");
         var data = JSON.stringify({ tracking: nextStep(singleCase.status), user_id: props.user.id ,date:date, deposit_12:singleCase.f2_deposit_12});
       }else if((singleCase.status === "book_received" )){
@@ -417,6 +417,8 @@ const Cases = (props) => {
       }else if((singleCase.status === "account_closing" )){
         console.log("incase 2");
         var data = JSON.stringify({ tracking: nextStep(nextStep(singleCase.status)), user_id: props.user.id ,date:date});
+      }else if (singleCase.status === 'book_received_back') {
+        var data = JSON.stringify({ tracking: nextStep(nextStep(singleCase.status)), user_id: props.user.id ,date:date ,deposit_12:singleCase.f2_deposit_12 });
       }else{
         console.log("incase 3");
         var data = JSON.stringify({ tracking: nextStep(singleCase.status), user_id: props.user.id ,date:date});
